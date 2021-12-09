b7c9158acbc5179cd26362dc81dc419c22645e17
christy welch <christinamawel@gmail.com>
2021-12-09 11:43:07 -0800

    add static method and property and pass tests for them
    Co-Authored-by: Zach Wilson <zwilson22495@gmail.com>

--- Scrabble.Tests/ModelTests/ScrabbleScoreTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleScoreTests.cs
@@ -14,11 +14,31 @@ namespace Scrabble.Models.Tests
       ScrabbleScore testScore = new ScrabbleScore("word");
       Assert.AreEqual(typeof(ScrabbleScore), testScore.GetType());
     }
-    // [TestMethod]
-    // public void GetScrabbleWord_ReturnsAString_String()
-    // {
-    //   ScrabbleScore testScore = "word";
-    //   Assert.AreEqual()
-    // }
+    [TestMethod]
+    public void GetScrabbleWord_ReturnsAString_String()
+    {
+      ScrabbleScore testScore = new ScrabbleScore("word");
+      Assert.AreEqual(typeof(String), testScore.Word.GetType());
+    }
+    [TestMethod]
+    public void GetScrabbleWord_ReturnsAString_ReturnsBunnies()
+    {
+      ScrabbleScore testScore = new ScrabbleScore("Bunnies");
+      Assert.AreEqual("Bunnies", testScore.Word);
+    }
+    [TestMethod]
+    public void GetLetterScore_ReturnsScoreForALetter_int()
+    {
+      Assert.AreEqual(ScrabbleScore.GetLetterScore("b"), 3);
+      Assert.AreEqual(ScrabbleScore.GetLetterScore("z"), 10);
+      Assert.AreEqual(ScrabbleScore.GetLetterScore("h"), 4);
+    }
+    [TestMethod]
+    public void GetLetterScore_ReturnsScoreForALetterInUpperCase_int()
+    {
+      Assert.AreEqual(ScrabbleScore.GetLetterScore("B"), 3);
+      Assert.AreEqual(ScrabbleScore.GetLetterScore("Z"), 10);
+      Assert.AreEqual(ScrabbleScore.GetLetterScore("H"), 4);
+    }
   }
 }
--- Scrabble/Models/ScrabbleScore.cs
+++ Scrabble/Models/ScrabbleScore.cs
@@ -5,11 +5,22 @@ namespace Scrabble.Models
 {
   public class ScrabbleScore
   {
-    public string Word;
+    public string Word { get; }
+    private static Dictionary<string, int> ScrabbleDictionary = new Dictionary<string, int>() 
+      {
+        {"a", 1}, {"b", 3}, {"c", 3}, {"d", 2}, {"e", 1}, {"f", 4}, {"g", 2}, {"h", 4}, {"i", 1}, {"j", 8}, {"k", 5}, {"l", 1}, {"m", 3}, {"n", 1}, {"o", 1}, {"p", 3}, {"q", 10}, {"r", 1}, {"s", 1}, {"t", 1}, {"u", 1}, {"v", 4}, {"w", 4}, {"x", 8}, {"y", 4}, {"z", 10}
+      };
 
     public ScrabbleScore(string word)
     {
       Word = word;
     }
+
+    public static int GetLetterScore(string letter)
+    {
+      return ScrabbleDictionary[letter.ToLower()];
+    }
+
+  
   }
 }
